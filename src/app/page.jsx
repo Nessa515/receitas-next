acdbe609acb1b8dc20c08b5bd0a544e106255386
Nessa515 <vanessaoliveiradasilva515@gmail.com>
2024-09-30 22:14:30 -0400

    Iniciando projeto next

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Cabecalho from "@/components/Cabecalho";
+import Card from "@/components/Card";
+
+export default function Home() {
+  return (
+    <div>
+      <Cabecalho />
+      <Card />
+    </div>
+  );
+}
