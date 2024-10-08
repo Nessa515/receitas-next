6ac1ee7141b0108af74598a5b2abf52a5d883003
Nessa515 <vanessaoliveiradasilva515@gmail.com>
2024-10-07 22:20:36 -0400

    Usando o useState

--- src/components/CardList/index.jsx
+++ src/components/CardList/index.jsx
@@ -1,15 +1,24 @@
+'use client'
+import { useState } from "react";
 import Card from "../Card";
 
 export default function CardList(){
+
+    const [cards, setCards] = useState([
+        {id: 1, titulo: "Ovo com arroz e pepino", tempo: 25, serve: 2},
+        {id: 2, titulo: "Ovo mexido",             tempo: 20, serve: 2},
+        {id: 3, titulo: "Ovo com macarrão",       tempo: 15, serve: 1},
+        {id: 4, titulo: "Pão com ovo",            tempo: 10, serve: 1},
+    ]);
+
     return(
-        <div className="flex flex-wrap">
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+        <div className="flex flex-wrap flex-col m-8">
+            <div>Quantidade de receitas: {cards.length}</div>
+            <ul className="list-disc">
+                {cards.map(c => (
+                    <li key={c.id}>{c.titulo} - serve {c.serve} pessoa(s)</li>
+                ))}
+            </ul>
         </div>
     )
 }
