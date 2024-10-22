4b6018b5201c9e9f4510c6306fe058b27948306d
Nessa515 <vanessaoliveiradasilva515@gmail.com>
2024-10-21 22:21:13 -0400

    Utilizando tecnologia strapi

--- src/components/CardList/index.jsx
+++ src/components/CardList/index.jsx
@@ -4,12 +4,7 @@ import Card from "../Card";
 
 export default function CardList(){
 
-    const [cards, setCards] = useState([
-        // {id: 1, titulo: "Ovo com arroz e pepino", imagem: "ovo-1.png", tempo: 25, serve: 2},
-        // {id: 2, titulo: "Ovo mexido",             imagem: "ovo-2.png", tempo: 20, serve: 2},
-        // {id: 3, titulo: "Ovo com macarrão",       imagem: "ovo-3.png", tempo: 15, serve: 1},
-        // {id: 4, titulo: "Pão com ovo",            imagem: "ovo-4.png", tempo: 10, serve: 1},
-    ]);
+    const [cards, setCards] = useState([]);
 
     useEffect(() => {
 
