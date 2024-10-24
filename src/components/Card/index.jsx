90141e6559a49d51dc0a9924d7e458e367127738
Nessa515 <vanessaoliveiradasilva515@gmail.com>
2024-10-23 21:36:49 -0400

    Adicionado imagens

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -6,10 +6,12 @@ export default function Card({ imagem, titulo, tempo, serve }){
         <div
             className="m-4 rounded border-2 border-rose-400 max-w-[387px]">
                 <Image
-                    src={`/${imagem}`}
+                    src={imagem}
                     width="387"
                     height="297"
-                    alt={titulo} />
+                    alt={titulo}
+                    className="min-h-72"
+                    />
             <h2 className="p-2 bg-rose-300 text-gray-800">{titulo}</h2>
             <div className="p-2 text-xs flex items-center gap-1">
                 <Timer /> {tempo} minuto(s)
--- src/components/CardList/index.jsx
+++ src/components/CardList/index.jsx
@@ -11,7 +11,7 @@ export default function CardList(){
         const bearerToken = process.env.NEXT_PUBLIC_API_TOKEN
 
         async function getData() {
-            await fetch("http://localhost:1337/api/receitas", {
+            await fetch("http://localhost:1337/api/receitas?populate=*", {
                 method: 'GET',
                 headers: {
                     'Authorization': `Bearer ${bearerToken}`,
@@ -32,7 +32,7 @@ export default function CardList(){
                 {cards.map(c => (
                     <Card
                         key = {c.id}
-                        imagem = {c.imagem}
+                        imagem = {`${process.env.NEXT_PUBLIC_STRAPI_URL}${c.Capa.formats.thumbnail.url}`}
                         titulo = {c.Titulo}
                         tempo = {c.Preparo}
                         serve = {c.Porcoes}
