54edc7a548c71042142ed524b2b82aca709ddd1c
Nessa515 <vanessaoliveiradasilva515@gmail.com>
2024-10-30 21:33:09 -0400

    Fazendo a estilização

--- src/components/Cabecalho/index.jsx
+++ src/components/Cabecalho/index.jsx
@@ -1,8 +1,13 @@
+import Image from "next/image"
+
+Image
+
 export default function Cabecalho() {
     return(
         <header
             className="bg-rose-400 text-center pt-8 pb-2"
         >
+        
             <h1>Fritando ovo</h1>
             <h1>Receitas fáceis</h1>
         </header>
